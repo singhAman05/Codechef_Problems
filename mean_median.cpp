e9d6f168b5698e2fa253a679de5626f198a652e0
Aman <amanshankarsingh05@gmail.com>
2022-09-10 17:22:01 +0530

    tenth commit

---
+++ mean_median.cpp
@@ -0,0 +1,11 @@
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+    float t;cin>>t;
+    while(t--){
+        float x,y;cin>>x>>y;
+        float s=(3*x)-2*y;
+        cout<<y<<" "<<y<<" "<<s<<endl;
+    }
+    return 0;
+}
